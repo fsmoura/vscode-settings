3559ca6f32c3eed6b6d652e155327c943287d006
Fabio Moura <fabio.siq.moura@gmail.com>
2020-12-01 07:35:47 -0300

    Initial commit

---
+++ vscode-extensions.sh
@@ -0,0 +1,30 @@
+code --install-extension adpyke.codesnap
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension dracula-theme.theme-dracula
+code --install-extension EditorConfig.EditorConfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension felixfbecker.php-intellisense
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension GraphQL.vscode-graphql
+code --install-extension JakeWilson.vscode-cdnjs
+code --install-extension jeff-hykin.better-dockerfile-syntax
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension syler.sass-indented
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscode-icons-team.vscode-icons
+
+#olaaaa
+code --install-extension adpyke.codesnap
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension dracula-theme.theme-dracula
+code --install-extension EditorConfig.EditorConfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension felixfbecker.php-intellisense
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension GraphQL.vscode-graphql
+code --install-extension JakeWilson.vscode-cdnjs
+code --install-extension jeff-hykin.better-dockerfile-syntax
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension syler.sass-indented
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscode-icons-team.vscode-icons
