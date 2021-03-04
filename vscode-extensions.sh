b34bee8423502b57ec6530d0ef15ce41d7496912
Fabio Moura <fabio.siq.moura@gmail.com>
2021-03-04 11:22:31 -0300

    Update vscode-extensions.sh

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,12 +1,11 @@
+#/bin/bash
+
 code --install-extension adpyke.codesnap
 code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension dracula-theme.theme-dracula
 code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
-code --install-extension felixfbecker.php-intellisense
 code --install-extension formulahendry.auto-rename-tag
-code --install-extension GraphQL.vscode-graphql
-code --install-extension JakeWilson.vscode-cdnjs
 code --install-extension jeff-hykin.better-dockerfile-syntax
 code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension syler.sass-indented
