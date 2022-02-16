fa43a25ae1aa2389309661e7539047c234b170bc
Fabio Siq <fabio.siq.moura@gmail.com>
2022-02-15 22:13:02 -0300

    Update vscode-extensions.sh

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -7,6 +7,9 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
 code --install-extension formulahendry.auto-rename-tag
 code --install-extension jeff-hykin.better-dockerfile-syntax
-code --install-extension syler.sass-indented
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension streetsidesoftware.code-spell-checker-portuguese-brazilian
 code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension vscode-icons-team.vscode-icons
+code --install-extension wix.vscode-import-cost
+code --install-extension xabikos.JavaScriptSnippets
